2c21b94012c8af13049ba0de3a0ca8628ae93569
yqb <1435705326@qq.com>
2026-01-15 17:01:32 +0800

    feat: 添加样式导入和调整权限表格组件的样式

---
+++ packages/components/permission-table/style/css.ts
@@ -0,0 +1,3 @@
+import '@element-plus/components/select/style/css'
+import '@element-plus/components/checkbox/style/css'
+import '@element-plus/theme-chalk/el-permission-table.css'
---
+++ packages/components/permission-table/style/index.ts
@@ -0,0 +1,3 @@
+import '@element-plus/components/select/style'
+import '@element-plus/components/checkbox/style'
+import '@element-plus/theme-chalk/src/permission-table.scss'
